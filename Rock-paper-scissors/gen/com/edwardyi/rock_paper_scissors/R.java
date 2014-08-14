5015bde028d975e91c775141ee8e661e6c7b2007
edwardyi <yi.wei.jie@hotmail.com>
2014-08-14 16:59:42 +0800

    剪刀石頭布完成
    剪刀石頭布完成

--- Rock-paper-scissors/gen/com/edwardyi/rock_paper_scissors/R.java
+++ Rock-paper-scissors/gen/com/edwardyi/rock_paper_scissors/R.java
@@ -1091,10 +1091,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050046;
+        public static final int action_settings=0x7f050048;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int btn_play=0x7f050046;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1148,6 +1149,7 @@ containing a value of this type.
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
+        public static final int txt_result=0x7f050047;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1251,12 +1253,13 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
-        public static final int btn_play=0x7f0a0013;
+        public static final int btn_play=0x7f0a0014;
+        public static final int game_title=0x7f0a0010;
         public static final int hello_world=0x7f0a000e;
-        public static final int paper=0x7f0a0010;
-        public static final int result=0x7f0a0014;
-        public static final int rock=0x7f0a0012;
-        public static final int scissor=0x7f0a0011;
+        public static final int paper=0x7f0a0011;
+        public static final int rock=0x7f0a0013;
+        public static final int scissor=0x7f0a0012;
+        public static final int txt_result=0x7f0a0015;
     }
     public static final class style {
         /** 
@@ -2635,7 +2638,7 @@ containing a value of this type.
          */
         public static final int[] MenuView = {
             0x010100ae, 0x0101012c, 0x0101012d, 0x0101012e,
-            0x0101012f, 0x01010130, 0x01010131, 0x01010438
+            0x0101012f, 0x01010130, 0x01010131, 0x010103f8
         };
         /**
           <p>
--- Rock-paper-scissors/src/com/edwardyi/rock_paper_scissors/MainActivity.java
+++ Rock-paper-scissors/src/com/edwardyi/rock_paper_scissors/MainActivity.java
@@ -4,15 +4,97 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class MainActivity extends ActionBarActivity {
-
+	private Button btn_play;
+	private RadioGroup rdg1;
+	private RadioGroup rdg2;
+	private TextView txt_result;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		findViews();
+		btn_play.setOnClickListener(btn_callback);
+		rdg1.setOnCheckedChangeListener(rdg_callback);
 	}
+	
+	private void findViews(){
+		btn_play = (Button)findViewById(R.id.btn_play);
+		rdg1 = (RadioGroup)findViewById(R.id.group1);
+		rdg2 = (RadioGroup)findViewById(R.id.group2);
+		txt_result = (TextView)findViewById(R.id.txt_result);
+	}
+//	點擊按鈕事件
+	public Button.OnClickListener btn_callback = new OnClickListener(){
+		@Override
+		public void onClick(View v) {
+			
+			// TODO Auto-generated method stub
+			int checkedId1 = rdg1.getCheckedRadioButtonId();
+			int checkedId2 = rdg2.getCheckedRadioButtonId();
+//			System.out.println(checkedId1+"|"+checkedId2);
+			if(checkedId1 == checkedId2 && checkedId1 != -1 && checkedId2 != -1){
+				txt_result.setText("在一次!");
+			}
+			if(checkedId1 == R.id.opt1_paper ){
+				if(checkedId2 == R.id.opt2_scissor){
+					txt_result.setText("player1輸了!");
+				}else if(checkedId2 == R.id.opt2_rock){
+					txt_result.setText("player1贏了!");
+				}else{
+					txt_result.setText("在一次!");
+				}
+			}else if(checkedId1 == R.id.opt1_rock){
+				if(checkedId2 == R.id.opt2_paper){
+					txt_result.setText("player1輸了!");
+				}else if(checkedId2 == R.id.opt2_scissor){
+					txt_result.setText("player1贏了!");
+				}else{
+					txt_result.setText("在一次!");
+				}
+			}else{
+				if(checkedId2 == R.id.opt2_rock){
+					txt_result.setText("player1輸了!");
+				}else if(checkedId2 == R.id.opt2_paper){
+					txt_result.setText("player1贏了!");
+				}else{
+					txt_result.setText("在一次!");
+				}
+			}
+			
+		}
+		
+	} ;
+//	點擊RadioButton事件
+	public RadioGroup.OnCheckedChangeListener rdg_callback = new OnCheckedChangeListener(){
 
+		@Override
+		public void onCheckedChanged(RadioGroup group, int checkedId) {
+			// TODO Auto-generated method stub
+//			判斷是否為group1
+//			System.out.println(group.getId()+"|"+R.id.group1+"|"+rdg2.getCheckedRadioButtonId());
+//			
+//		
+//			if(group.getId() == R.id.group1){
+////			判斷group1和group2是否相同
+//				int checkedId2 = rdg2.getCheckedRadioButtonId();
+//				if(checkedId == checkedId2){
+//					txt_result.setText("在一次!");
+//				}
+//				
+//			}
+		}
+		
+	};
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
